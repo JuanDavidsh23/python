fe8123518e4ef5c6989387092e403b65655328f1
juandavidsh23 <santamariajuan34@gmail.com>
2025-11-07 10:48:20 -0500

    new updates

--- buclesRepeticion.py
+++ buclesRepeticion.py
@@ -1,50 +1,67 @@
-print("contar de 1 a 10")
-for i in range(1, 11):
-    print(i)
+print("BIENVENIDO A BUCLES Y REPETICIONES")
 
-""" --------------------------------------- """
+while True:
+    print(" 1.NUMERO DEL 1 AL 10 \n 2.SUmatoria del 1 al N \n 3.Tabla de multiplicar \n 4.contador regresivo con while \n 5.Adivina el numero \n 6.sumar hasta que escriba 0 \n 7.Salir")
+    opt = int(input("ingrese una opcion para realizar: "))
 
-print("sumatoria de números del 1 al n")
-n = int(input("Ingrese un número entero positivo: "))
-sumatoria = 0
-for i in range(1, n + 1):
-    sumatoria += i
-print(f"La sumatoria de los números del 1 al {n} es: {sumatoria}")
+    if opt == 1:
+        print("contar de 1 a 10")
+        for i in range(1, 11):
+            print(i)        
 
-print("Tabla de multiplicar")
-num = int(input("Ingrese un número para ver su tabla de multiplicar: "))
-for i in range(1, 11):
-    print(f"{num} x {i} = {num * i}")
+# --------------------------------------- """
+    elif opt == 2:
+        print("sumatoria de números del 1 al n")
+        n = int(input("Ingrese un número entero positivo: "))
+        sumatoria = 0
+        for i in range(1, n + 1):
+            sumatoria += i
+        print(f"La sumatoria de los números del 1 al {n} es: {sumatoria}")
 
-""" --------------------------------------- """
+#--------------------------------------- """
 
-print("contador regresivo con while")
-contador = 10
-while contador > 0:
-    print(contador)
-    contador -= 1
-print("Fin del contador regresivo.")
+    elif opt == 3:
+        print("Tabla de multiplicar")
+        num = int(input("Ingrese un número para ver su tabla de multiplicar: "))
+        for i in range(1, 11):
+            print(f"{num} x {i} = {num * i}")
 
-""" --------------------------------------- """
+#--------------------------------------- """
+    elif opt == 4:
+        print("contador regresivo con while")
+        contador = 10
+        while contador > 0:
+            print(contador)
+            contador -= 1
+        print("Fin del contador regresivo.")
 
-print("adivina el número con random")
-import random
-opt = random.choice(range(1, 101))
-player = int(input("Adivina el número entre 1 y 100: "))
-while player != opt:
-    if player < opt:
-        print("Demasiado bajo. Intenta de nuevo.")
+#--------------------------------------- """
+    elif opt == 5:
+        print("adivina el número con random")
+        import random
+        opt = random.choice(range(1, 101))
+        player = int(input("Adivina el número entre 1 y 100: "))
+        while player != opt:
+            if player < opt:
+                print("Demasiado bajo. Intenta de nuevo.\n")
+            else:
+                print("Demasiado alto. Intenta de nuevo. \n")
+            player = int(input("Adivina el número entre 1 y 100: "))
+        print("¡Felicidades! Has adivinado el número.")
+
+#--------------------------------------- """
+    elif opt == 6:
+        print("sumar hasta que user escriba 0")
+        userNum = int(input("Ingrese un número (0 para terminar): "))
+        totalSum = 0
+        while userNum != 0:
+            totalSum += userNum
+            userNum = int(input("Ingrese un número (0 para terminar): "))
+        print(f"La suma total es: {totalSum}")
+
+#--------------------------------------- """
+    elif opt == 7: 
+        print("Adios")
+        break
     else:
-        print("Demasiado alto. Intenta de nuevo.")
-    player = int(input("Adivina el número entre 1 y 100: "))
-print("¡Felicidades! Has adivinado el número.")
-
-print("sumar hasta que user escriba 0")
-userNum = int(input("Ingrese un número (0 para terminar): "))
-totalSum = 0
-while userNum != 0:
-    totalSum += userNum
-    userNum = int(input("Ingrese un número (0 para terminar): "))
-print(f"La suma total es: {totalSum}")
-
-""" --------------------------------------- """
+        print("ELija una numero valido. \n")
--- condicionales.py
+++ condicionales.py
@@ -1,73 +1,91 @@
 
+print("BIENVENIDO A CONDICIONALES.\n")
+while True:
+    print(" 1.Mayor de edad \n 2.Número positivo, negativo o cero. \n 3.Calculadora básica con +, -, *, \n 4.Clasificador de notas (Excelente, Aprobado, Reprobado). /. \n 5.Comparador de numeros \n 6.par e impar. \n 7.Salir \n")
 
+    opt = int(input("Ingrese una opcion:"))
+    if opt == 1:
+        print("mayor o menor de edad")
+        edad = int(input("Ingrese su edad: "))
+        if edad >= 18:
+            print("Eres mayor de edad.")
+        else:
+            print("Eres menor de edad.")
+            print("Recuerda que la mayoría de edad es a los 18 años.")  
 
-print("mayor o menor de edad")
-edad = int(input("Ingrese su edad: "))
-if edad >= 18:
-    print("Eres mayor de edad.")
-else:
-    print("Eres menor de edad.")
-    print("Recuerda que la mayoría de edad es a los 18 años.")  
+    # --------------------------------------- """
+    elif opt == 2:
+        print("número positivo, negativo o cero")
+        numero = float(input("Ingrese un número: "))
+        if numero > 0:
+            print("El número es positivo.")
+        elif numero < 0:
+            print("El número es negativo.")
+        else:
+            print("El número es cero.")
 
-""" --------------------------------------- """
+    # --------------------------------------- """
+    elif opt == 3: 
+        print("calculadora basica con +, -, *, /")
+        num1 = float(input("Ingrese el primer número: "))
+        num2 = float(input("Ingrese el segundo número: "))
+        operacion = input("Ingrese la operación (+, -, *, /): ")    
+        if operacion == "+":
+            resultado = num1 + num2
+            print(f"El resultado de {num1} + {num2} es: {resultado}")
+        elif operacion == "-":
+            resultado = num1 - num2
+            print(f"El resultado de {num1} - {num2} es: {resultado}")
+        elif operacion == "*":
+            resultado = num1 * num2
+            print(f"El resultado de {num1} * {num2} es: {resultado}")
+        elif operacion == "/":
+            if num2 != 0:
+                resultado = num1 / num2
+                print(f"El resultado de {num1} / {num2} es: {resultado}")
+            else:
+                print("Error: No se puede dividir por cero.")
+        else:
+            print("Operación no válida.")
 
-print("número positivo, negativo o cero")
-numero = float(input("Ingrese un número: "))
-if numero > 0:
-    print("El número es positivo.")
-elif numero < 0:
-    print("El número es negativo.")
-else:
-    print("El número es cero.")
+    #--------------------------------------- """
+    elif opt == 4:
+        print("clasficar notas excelente, bueno, regular, insuficiente")
+        nota = float(input("Ingrese la nota: "))
+        if nota >= 90:
+            print("La nota es excelente.")
+        elif nota >= 75:
+            print("La nota es buena.")
+        elif nota >= 60:
+            print("La nota es regular.")
+        else:
+            print("La nota es insuficiente.")
 
-""" --------------------------------------- """
+    #--------------------------------------- """
+    elif opt == 5:
+        print("comparador de números")
 
-print("calculadora basica con +, -, *, /")
-num1 = float(input("Ingrese el primer número: "))
-num2 = float(input("Ingrese el segundo número: "))
-operacion = input("Ingrese la operación (+, -, *, /): ")    
-if operacion == "+":
-    resultado = num1 + num2
-    print(f"El resultado de {num1} + {num2} es: {resultado}")
-elif operacion == "-":
-    resultado = num1 - num2
-    print(f"El resultado de {num1} - {num2} es: {resultado}")
-elif operacion == "*":
-    resultado = num1 * num2
-    print(f"El resultado de {num1} * {num2} es: {resultado}")
-elif operacion == "/":
-    if num2 != 0:
-        resultado = num1 / num2
-        print(f"El resultado de {num1} / {num2} es: {resultado}")
+        num1 = float(input("Ingrese el primer número: "))
+        num2 = float(input("Ingrese el segundo número: "))
+        if num1 > num2:
+            print(f"{num1} es mayor que {num2}.")
+        elif num1 < num2:
+            print(f"{num1} es menor que {num2}.")
+        else:
+            print(f"{num1} es igual a {num2}.")
+
+    #--------------------------------------- """
+
+    elif opt == 6:
+        print("NUmero par o impar ")
+        num = int(input("Ingrese un numero: "))
+        if num % 2 == 0:
+            print("Numero par")
+        else: 
+            print("Numero impar")
+
+    elif opt == 7:
+        print("Adios")
+        break
     else:
-        print("Error: No se puede dividir por cero.")
-else:
-    print("Operación no válida.")
-
-""" --------------------------------------- """
-
-print("clasficar notas excelente, bueno, regular, insuficiente")
-nota = float(input("Ingrese la nota: "))
-if nota >= 90:
-    print("La nota es excelente.")
-elif nota >= 75:
-    print("La nota es buena.")
-elif nota >= 60:
-    print("La nota es regular.")
-else:
-    print("La nota es insuficiente.")
-
-""" --------------------------------------- """
-
-print("comparador de números")
-
-num1 = float(input("Ingrese el primer número: "))
-num2 = float(input("Ingrese el segundo número: "))
-if num1 > num2:
-    print(f"{num1} es mayor que {num2}.")
-elif num1 < num2:
-    print(f"{num1} es menor que {num2}.")
-else:
-    print(f"{num1} es igual a {num2}.")
-
-""" --------------------------------------- """
+        print("ELija una opcion valida ")    

8a2d103aeb89a99bc93e76663da7923090a9896e
juandavidsh23 <santamariajuan34@gmail.com>
2025-11-10 11:51:42 -0500

    userHistory2 complete

--- UserHistory.py/userHistory2.py
+++ UserHistory.py/userHistory2.py
@@ -2,40 +2,53 @@ import time
 print("Invetario completo")
 
 inventory = {}
-while True:
+productsCount  = 0
+priceCount = 0
+def ShowMenu ():
     print(" 1.Agregar productos\n 2.Mostrar inventario\n 3.Calcular estadisticas\n 4.salir")
-    while True: 
-        try:            
-            options = int(input("ingrese una opcion del menu: "))
-            if options <1 or options >4:
-                print("ERROR, la opcion debe ser entre 1 y 4\n")
+
+def validateOption():
+        while True:
+            try:            
+                options = int(input("ingrese una opcion del menu: "))
+                if options <1 or options >4:
+                    print("ERROR, la opcion debe ser entre 1 y 4\n")
+                    continue
+                return options
+            except ValueError:
+                print("ERROR, la opcion debe ser un valor numerico\n")
                 continue
-        except ValueError:
-            print("ERROR, la opcion debe ser un valor numerico\n")
-            continue
 
-        if options == 1:
-            print("Agregue sus productos")
-            name = input("Ingrese el nombre del producto: ")
-            while True:
-                try: 
-                    price = float(input("Ingrese el precio del producto: "))
-                    quantity = int(input("Ingrese la cantidad que desea comprar: "))
-                    break
-                except ValueError: 
-                    print("La Cantidad o el precio deben ser datos numericos")
+def addProducts():
+    global priceCount, productsCount
+    while True:          
+        print("Agregue sus productos")
+        name = input("Ingrese el nombre del producto: ")
+        if name.replace("" , "").isalpha():
+              break
+        else: 
+              print("El nombre no puede tener numeros.\n")
+    while True:
+            try: 
+                price = float(input("Ingrese el precio del producto: "))
+                priceCount += price
+                quantity = int(input("Ingrese la cantidad que desea comprar: "))
+                productsCount += quantity
+                break
+            except ValueError: 
+                print("La Cantidad o el precio deben ser datos numericos")
             
-            if name in inventory:
-                inventory['quantity']  += quantity 
+    if name in inventory:
+                    inventory[name]['quantity'] += quantity  
 
-            else:
-                inventory[name] = {'price' : price , 'quantity' : quantity}
+    else:
+                    inventory[name] = {'price': price, 'quantity': quantity}
 
-        if options == 2:
-            print("    Cargando productos..", end="", flush=True)
+def showProducts():
+            print("    Cargando productos..", end="")
             for i in range(1,101):
-                print(f"\r{i}%" , end = " " , flush=True)
-                time.sleep(0.08)
+                print(f"\r{i}%" , end = " " )
+                time.sleep(0.02)
             print()
 
             while True:
@@ -50,3 +63,38 @@ while True:
                 except ValueError as e: 
                     print(f"No hay productos para mostrar {e}")
                     break
+def stadistics():
+           total = 0
+           while True:
+            try:
+                if not inventory:
+                      raise ValueError ("Nada en el inventario")
+                
+
+                for name,details in inventory.items():
+                        totalCost = details['price'] * details['quantity']
+                        print(f"producto: {name}: precio: {details['price']} x cantidad: {details['quantity']} = {totalCost}")
+                        total += totalCost
+                print(f"Agregaste {productsCount} productos  con un precio de: {total} ")
+                break
+
+            except ValueError:
+                print("No hay nada en el inventario\n")
+                break
+
+
+while True:
+    ShowMenu()
+    option = validateOption()
+
+    if option == 1:
+        addProducts()
+    
+    elif option == 2:
+        showProducts()
+
+    elif option == 3:
+        stadistics()
+    else:
+          print("Gracias por usar el sistema.")
+          break
